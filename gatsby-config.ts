6b6aa80e2d8f1bf193921ece73b8f2354e6960de
adamatronix <you@example.com>
2022-04-20 14:33:16 +0800

    master Add all sorts of goodies

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,11 +1,33 @@
 import type { GatsbyConfig } from "gatsby";
+import path from 'path';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Gatsby Boilerplate`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-image",
+    "gatsby-plugin-offline",
+    "gatsby-plugin-resolve-src",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.resolve(`src/images`),
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /.svg$/,
+        }
+      }
+    },
+  ]
 };
 
 export default config;
